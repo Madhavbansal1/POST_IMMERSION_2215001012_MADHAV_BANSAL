987b39e957540c6753048ccaea57099fd27bcbae
Madhav Bansal <122335374+Madhavbansal1@users.noreply.github.com>
2025-07-30 11:09:47 +0530

    Create Pseudo_code_2.java

---
+++ DAY1/Pseudo_code_2.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        while (a < 5) {
+            a = a + 2;
+            
+        }
+      System.out.println("a = " + a);//5
+    }
+}
+
+// while loop run for 2 times
